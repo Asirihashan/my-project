e1ed22433b0b74af782abc3dde5f4b035148e777
Asiri <asirihashanuni456@gmail.com>
2024-05-21 22:05:49 +0530

    added new branch

---
+++ funtion.py
@@ -0,0 +1,2 @@
+def greeting_function ():
+    return "Welcome to my git project."
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from funtion import greeting_function
+
 def greet():
     return "Hello, World!"
 
 if __name__ == "__main__":
     print(greet())
+    print(greeting_function())
 
