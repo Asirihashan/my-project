5424e328939488dee61485253eb2f212776cf760
Duvindu Sahan <100335279+duvindusahan@users.noreply.github.com>
2024-05-21 22:31:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from funtion import greeting_function
 def greet():
     return "Hello, World!"
 
+def wish():
+    return "Have a nice day!!"
+
 if __name__ == "__main__":
     print(greet())
     print(greeting_function())
+    print(wish())
 
